3294bf1a31725333ed1688c96e4d40a25163b38d
0dminnimda <0dminnimda@gmail.com>
2023-09-13 09:11:10 +0300

    Allow for files in different directories for prep_exec.sh
    So, don't copy built binaries, use them directly

--- prep_exec.sh
+++ prep_exec.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ $OSTYPE =~ "android" ]]; then
-    cp -f $1 ~/$1
-    chmod +x ~/$1
+NAME=$(basename "$1")
 
-    echo "~/$1"
+if [[ $OSTYPE =~ "android" ]]; then
+    cp -f $1 ~/$NAME
+    chmod +x ~/$NAME
+
+    echo "~/$NAME"
 else
     echo "$1"
 fi
