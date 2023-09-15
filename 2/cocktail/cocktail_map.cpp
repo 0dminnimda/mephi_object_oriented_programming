ec15411b060d3acf3fe97788fd8ad5b577abdde8
0dminnimda <0dminnimda@gmail.com>
2023-09-15 21:07:42 +0300

    2\cocktail\cocktail_map.cpp: fix operator<< func

--- 2/cocktail/cocktail_map.cpp
+++ 2/cocktail/cocktail_map.cpp
@@ -100,7 +100,7 @@ public:
             if (!first) {
                 stream << ", ";
             }
-            first = !first;
+            first = false;
 
             stream << it.first() << ": " << it.second();
         }
