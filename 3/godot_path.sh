f75d3f83f33e808354f323ecee1d0caeb650d5e3
0dminnimda <0dminnimda@gmail.com>
2023-10-29 14:39:48 +0300

    3\godot_path.sh: return something for other platforms

--- 3/godot_path.sh
+++ 3/godot_path.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ $OSTYPE =~ "android" ]]; then
-    echo "Oops"
-elif [[ $OSTYPE =~ "msys" ]]; then
+if [[ $OSTYPE =~ "msys" ]]; then
     echo "C:\Godot\Godot_v4.1.1-stable_win64\Godot_v4.1.1-stable_win64_console.exe"
 else
-    echo "Oops"
+    echo "godot"
 fi
