8e76e038cb98bfac7df8ea2fd63401385dc65d1a
0dminnimda <0dminnimda@gmail.com>
2023-10-31 01:40:56 +0300

    3\design.py: expand to capture most of the game

--- 3/design.py
+++ 3/design.py
@@ -1,4 +1,30 @@
 Vector2
+Sprite2D
+
+
+class Game:
+    current_level_index: int
+    all_levels: list[DungeonLevel]
+
+
+class DungeonLevel:
+    all_actors: list[Actor]
+    tiles: list[list[Tile]]
+
+
+class Tile:
+    enum Kind:
+        ...
+
+    kind: Kind
+
+    laying_items: list[Item]
+    building: Building
+
+
+class Building:
+    pass
+    # like draw() but already in godot
 
 
 class Experience:
@@ -8,20 +34,50 @@ class Experience:
     def gain(amount: int) -> None: ...
 
 
-class Health:
-    value: float
-    maximum: float
-
-
 class ActorClass:
     ...
+
+
+class Equipment:
+    wearable: dict[Wearable::Kind, Wearable | None]
+    weapons: list[Weapon]
+
+
+class Characteristics:
+    max_health: float
+    defence: float
+
+
+class ValueModifier:
+    def apply(value: T) -> T: ...
+
+
+class SetAbsoluteValue(ValueModifier):
+    value: T
+
+    def apply(value: T) -> T: ...
+
+
+class AddToValue(ValueModifier):
+    value: T
+
+    def apply(value: T) -> T: ...
+
+
+# ValueModifier = SetAbsoluteValue | AddToValue
+
+
+class CharacteristicsModifier:
+    max_health: ValueModifier<float>
+    defence: ValueModifier<float>
 
 
 class Actor:  # XXX: creature?
     actor_class: ActorClass
     position: Vector2
-    health: Health
-    defence: float
+    health: float
+    characteristics: Characteristics
+    equipment: Equipment
 
     def chance_to_take_damage() -> float: ...
     def take_damage(amount: float, source: Actor): ...
@@ -33,25 +89,23 @@ class Inventory:
     items: list[Item]
 
 
-class Equipment:
-    items: dict[Wearable::Kind, Wearable | None]
-
-
 class Player(Actor):
-    equipment: Equipment
     inventory: Inventory
     experience: Experience
 
 
 class Enemy(Actor):
-    weapon: Weapon
-
-
-class Item:
     pass
 
 
+class Item:
+    name: str
+    icon: Sprite2D
+
+
 class Potion(Item):
+    modifier: CharacteristicsModifier
+
     def apply(target: Actor): ...
 
 
@@ -61,13 +115,23 @@ class SpacialDistribution:
     def eval_at(pos: Vector2) -> bool: ...
 
 
+class Enchantment:
+    target_actor_class: ActorClass
+    damage_multiplier: float
+
+
 class Weapon(Item):
+    modifier: CharacteristicsModifier | None
+    enchantment: Enchantment | None
     distribution: SpacialDistribution
 
-    def attack(pos: Vector2, ...all_actors?): ...
+    def attack(pos: Vector2): ...
 
 
 class Wearable(Item):  # aka armour or equipment
+    modifier: CharacteristicsModifier | None
+    distribution: SpacialDistribution
+
     enum Kind:
         ...
 
@@ -75,8 +139,16 @@ class Wearable(Item):  # aka armour or equipment
 
 
 class LockPicks(Item):
-    pass
+    count: int
 
 
-class Chest:
+class LockPickingResult:
+    lock_picked: bool
+    pick_borken: bool
+
+
+class Chest(Building):
     inventory: Inventory
+    level: int
+
+    def try_to_pick(picks: LockPicks) -> LockPickingResult: ...
