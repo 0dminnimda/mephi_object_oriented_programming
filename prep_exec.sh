addacebe8d7535f1686fcaa748a028d947763b27
0dminnimda <0dminnimda@gmail.com>
2023-11-20 13:26:26 +0300

    Setup gui for termux

--- prep_exec.sh
+++ prep_exec.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 NAME=$(basename "$1")
+DIR=$(dirname "$1")
+USE_DIR=$(( $2 == "--use_dir" ? 1 : 0 ))
+
 
 if [[ $OSTYPE =~ "android" ]]; then
-    cp -f $1 ~/$NAME
+    if [[ $USE_DIR ]]; then
+        cp -f -r $DIR ~/
+    else
+        cp -f $1 ~/$NAME
+    fi
     chmod +x ~/$NAME
 
     echo "~/$NAME"
