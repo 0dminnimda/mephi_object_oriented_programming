45fe8eb5ac214b0c5e776d92d4fffa11783dddd4
0dminnimda <0dminnimda@gmail.com>
2023-11-26 19:15:35 +0300

    3\fix_puml.py: fix so it would not remove all text

--- 3/fix_puml.py
+++ 3/fix_puml.py
@@ -11,7 +11,5 @@ text = uml.read_text("utf-8")
 text = text.replace("std::", "std++")
 text = text.replace("::", "__")
 text = text.replace("std++", "std::")
-text = re.sub(text, r"^.+GDCLASS.+$", "")
-text = text.replace("std++", "std::")
 
 uml.write_text(text, "utf-8")
