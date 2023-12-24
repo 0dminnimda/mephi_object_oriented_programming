c47728e23e7c5464bba0d1daca89a9c3b3b82ad0
0dminnimda <0dminnimda@gmail.com>
2023-12-24 14:15:12 +0300

    3\src\sword.cpp: move some impls into class

--- 3/src/sword.cpp
+++ 3/src/sword.cpp
@@ -16,9 +16,11 @@ public:
         : MeleeWeapon(item_class_index, damage_range, push_back_force_multiplier, cooldown_time),
           hit_range(hit_range) {}
 
-    std::shared_ptr<Item> deepcopy_item() const override;
+    std::shared_ptr<Item> deepcopy_item() const override { return deepcopy_shared(*this); }
 
-    bool is_in_range(const Actor &source, sf::Vector2f target) const override;
+    bool is_in_range(const Actor &source, sf::Vector2f target) const override {
+        return length_squared(source.position - target) <= hit_range * hit_range;
+    }
 
 private:
     friend class boost::serialization::access;
@@ -32,16 +34,10 @@ private:
 
 BOOST_CLASS_EXPORT_KEY(Sword);
 
-bool Sword::is_in_range(const Actor &source, sf::Vector2f target) const {
-    return length_squared(source.position - target) <= hit_range * hit_range;
-}
-
 void Sword::deepcopy_to(Sword &other) const {
     MeleeWeapon::deepcopy_to(other);
     other.hit_range = hit_range;
 }
-
-std::shared_ptr<Item> Sword::deepcopy_item() const { return deepcopy_shared(*this); }
 
 class SwordPlugin : public ItemPlugin {
 public:
@@ -53,9 +49,9 @@ public:
         );
 
         std::vector<ItemPlugin::item_type> result;
-        result.push_back(std::move(
-            make_item<Sword>(sword, 0, RangeOfLong(3, 5), 2.0f, 10.0f, sf::seconds(0.3f))
-        ));
+        result.push_back(
+            std::move(make_item<Sword>(sword, 0, RangeOfLong(3, 5), 2.0f, 10.0f, sf::seconds(0.3f)))
+        );
         return result;
     }
 };
